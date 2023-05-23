d89a593fbb8f05c100d8240a1b6b9e7393c34bfe
arieslee <arieslee@student.ubc.ca>
2023-05-22 18:17:53 -0700

    added translink api

--- gobc/src/App.js
+++ gobc/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+headers.append("Accept", "application/json");
+
+function Schedule({routeName, direction, destination}) {
+  return (
+    <div>
+      <h1>{routeName}</h1>
+      <p>{direction} : {destination}</p>
+    </div>
+  )
+}
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://api.translink.ca/rttiapi/v1/stops/58624/estimates?apikey=MfbIeYyUTRdAUbp20RRP', {headers})
+    .then((response) => response.json())
+    .then(setData)
+    .then(() => {setLoading(false)})
+    .catch(setError);
+  }, []);
+  
+  if (loading) return <h1>Loading Data</h1>
+  if (error) return <pre>{JSON.stringify(error)}</pre>
+  if (!data) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  )
 }
 
 export default App;

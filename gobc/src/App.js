2691b929e9886a785a70a498e70c7e66b3596ffd
arieslee <arieslee@student.ubc.ca>
2023-05-22 19:12:36 -0700

    added user input feature

--- gobc/src/App.js
+++ gobc/src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import { useState, useEffect } from "react";
 
-
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
-headers.append("Accept", "application/json");
-
 function Schedule({routeName, direction, destination}) {
   return (
     <div>
@@ -15,7 +10,15 @@ function Schedule({routeName, direction, destination}) {
   )
 }
 
-function App() {
+function UserInput() {
+
+}
+
+function GetData() {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -35,6 +38,29 @@ function App() {
 
   return (
     <pre>{JSON.stringify(data, null, 2)}</pre>
+  )
+}
+
+function App() {
+  // const [data, setData] = useState(null);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch('https://api.translink.ca/rttiapi/v1/stops/58624/estimates?apikey=MfbIeYyUTRdAUbp20RRP', {headers})
+  //   .then((response) => response.json())
+  //   .then(setData)
+  //   .then(() => {setLoading(false)})
+  //   .catch(setError);
+  // }, []);
+  
+  // if (loading) return <h1>Loading Data</h1>
+  // if (error) return <pre>{JSON.stringify(error)}</pre>
+  // if (!data) return null;
+
+  return (
+    <GetData />
   )
 }
 

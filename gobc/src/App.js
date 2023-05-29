50f06f5225972f8d0fe611198d1b31df5510067d
arieslee <arieslee@student.ubc.ca>
2023-05-28 18:07:30 -0700

    fixed url building

--- gobc/src/App.js
+++ gobc/src/App.js
@@ -1,47 +1,28 @@
 import './App.css';
 import { useState, useEffect } from "react";
 
-function Schedule({routeName, direction, destination}) {
-  return (
-    <div>
-      <h1>{routeName}</h1>
-      <p>{direction} : {destination}</p>
-    </div>
-  )
-}
 
-function UserInput() {
-  return (
-    <div>
-      <h1>Where would you like to go today?</h1>
-      <input id="bnum" type="text" placeholder="Bus Stop Number (e.g #58624)"></input>
-      <input id= "bname" type="text" placeholder="Route Number(e.g 403)"></input>
-      <button onClick={() => {
-        GetData();
-        return false;
-      }}>Check Schedule</button>
-    </div>
-  )
-}
-
-function GetData() {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  headers.append("Accept", "application/json");
-
+function GetData({busStop}) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Accept", "application/json");
 
-    fetch('https://api.translink.ca/rttiapi/v1/stops/58624/estimates?apikey=MfbIeYyUTRdAUbp20RRP', {headers})
+    setLoading(true);
+    const BASE_URL = "https://api.translink.ca"
+    const SEARCH_PATH = busStop;
+    let FINAL_URL = `${BASE_URL}/rttiapi/v1/stops/${SEARCH_PATH}/estimates?apikey=MfbIeYyUTRdAUbp20RRP`;
+
+    fetch(FINAL_URL, {headers}) 
     .then((response) => response.json())
     .then(setData)
     .then(() => {setLoading(false)})
     .catch(setError);
-  }, []);
+  }, [busStop]);
   
   if (loading) return <h1>Loading Data</h1>
   if (error) return <pre>{JSON.stringify(error)}</pre>
@@ -53,25 +34,30 @@ function GetData() {
 }
 
 function App() {
-  // const [data, setData] = useState(null);
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  //58624
+  const [input, setInput] = useState('');
+  const [updated, setUpdated] = useState(input);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetch('https://api.translink.ca/rttiapi/v1/stops/58624/estimates?apikey=MfbIeYyUTRdAUbp20RRP', {headers})
-  //   .then((response) => response.json())
-  //   .then(setData)
-  //   .then(() => {setLoading(false)})
-  //   .catch(setError);
-  // }, []);
-  
-  // if (loading) return <h1>Loading Data</h1>
-  // if (error) return <pre>{JSON.stringify(error)}</pre>
-  // if (!data) return null;
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUpdated(input);
+  };
 
   return (
-    <UserInput />
+    <div>
+      <h1 className='App'>Where would you like to go today?</h1>
+      <form className='App'  onSubmit={handleSubmit}>
+        <input id="bnum" type="text" value={input} placeholder="Bus Stop Number" onChange={handleChange} />
+        <input type="submit" value="Check Schedule"/> 
+      </form>
+      <GetData busStop={updated}/>
+    </div>
+
+
   )
 }
 
